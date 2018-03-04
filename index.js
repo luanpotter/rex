6ff55fd46c9c493429ccf58e19967b4b1813404c
Luan Nico <luanpotter27@gmail.com>
2018-03-04 10:53:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const stdin = require('get-stdin');
+
+if (process.argv.length <= 3) {
+	console.error("Usage: echo 'data' | rex 'from' 'to'");
+	return;
+}
+
+const fromRaw = process.argv[2];
+const to = process.argv[3];
+
+const from = new RegExp(fromRaw);
+
+stdin().then(str => {
+	const result = str.replace(from, to);
+	process.stdout.write(result);
+});
